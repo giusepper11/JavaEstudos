fad48c9f80083307b0a061d96d475a0882cd2bad
Giuseppe Rosa <giuseppe.rosa@amcom.com.br>
2019-03-20 08:42:20 -0300

    Herancas com java

--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -1,12 +1,15 @@
+
 public class TesteFuncionario {
-    public static void main(String[] args) {
-        Funcionario peppe = new Funcionario();
-        peppe.setNome("giuseppe rosa");
-        peppe.setCpf("123456789-02");
-        peppe.setSalario(2500.80);
 
-        System.out.println(peppe.getNome());
-        System.out.println(peppe.getBonificacao());
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("nico steppat");
+		nico.setCpf("223355646-9");
+		nico.setSalario(2500.80);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+	}
 
-    }
 }
